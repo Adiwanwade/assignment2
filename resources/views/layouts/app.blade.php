f316154a9f734ef8370fd85b6eb493876fcb8271
Aditya Wanwade <Adiwanwade@gmail.com>
2023-12-26 11:13:19 +0530

    Fixed text error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white shadow">
+                <header class="bg-black shadow" style="color: black">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8" style="color: #B0976D">
                         {{ $header }}
                     </div>
